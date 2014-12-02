7dbbff9a73914248a14ff6f0757ff663afe73445
Hyunje Alex Jun <me@noraesae.net>
2014-12-02 20:08:01 +0000

    Add frontend jsx build task.

---
+++ frontend/scripts/index.jsx
@@ -0,0 +1,9 @@
+var React = require('react/react-with-addons');
+
+var App = React.createClass({
+  render: function () {
+    return <h1>Hello, world!</h1>
+  }
+});
+
+React.render(<App />, document.getElementById('app'));
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,33 @@
 var gulp = require('gulp')
+  , browserify = require('browserify')
   , connect = require('gulp-connect')
-  , rimraf = require('gulp-rimraf');
+  , rimraf = require('gulp-rimraf')
+  , rename = require('gulp-rename')
+  , transform = require('vinyl-transform')
+  , through = require('through')
+  , uglify = require('gulp-uglify');
+
+var isDist = process.argv.indexOf('serve') === -1;
 
 gulp.task('html', ['clean:html'], function () {
   return gulp.src('frontend/index.html')
     .pipe(gulp.dest('out'))
+    .pipe(connect.reload());
+});
+
+gulp.task('js', ['clean:js'], function () {
+   var browserified = transform(function(filename) {
+     var b = browserify(filename);
+     b.transform('reactify');
+     b.transform('debowerify');
+     return b.bundle();
+   });
+
+  return gulp.src('frontend/scripts/index.jsx')
+    .pipe(browserified)
+    .pipe(isDist ? uglify() : through())
+    .pipe(rename('build.js'))
+    .pipe(gulp.dest('out/js'))
     .pipe(connect.reload());
 });
 
@@ -18,6 +41,11 @@ gulp.task('clean:html', function () {
     .pipe(rimraf());
 });
 
+gulp.task('clean:js', function () {
+  return gulp.src('out/js/*')
+    .pipe(rimraf());
+});
+
 gulp.task('connect', ['build'], function () {
   connect.server({
     root: 'out',
@@ -27,8 +55,9 @@ gulp.task('connect', ['build'], function () {
 
 gulp.task('watch', function () {
   gulp.watch(['frontend/index.html'], ['html']);
+  gulp.watch(['frontend/scripts/**/*'], ['js']);
 });
 
-gulp.task('build', ['clean', 'html']);
+gulp.task('build', ['clean', 'html', 'js']);
 gulp.task('serve', ['connect', 'watch']);
 gulp.task('default', ['build']);
