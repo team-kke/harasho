ea50f62e9d3c07dff93446b875a79fb24125d7a4
Hyunje Alex Jun <me@noraesae.net>
2014-12-02 19:07:43 +0000

    Add html task into gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,19 @@
-var gulp = require('gulp');
+var gulp = require('gulp')
+  , rimraf = require('gulp-rimraf');
+
+gulp.task('html', function () {
+  return gulp.src('frontend/index.html')
+    .pipe(gulp.dest('out/index.html'));
+});
+
+gulp.task('clean', function () {
+  return gulp.src('out')
+    .pipe(rimraf());
+});
+
+gulp.task('clean:html', function () {
+  return gulp.src('out/index.html')
+    .pipe(rimraf());
+});
 
 gulp.task('default', []);
